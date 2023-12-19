e8a8955be19ec3bf271ed7d2e467f56deb480e46
vishnuvardhan <134271319+vishnuvardhan2114@users.noreply.github.com>
2023-12-19 15:54:39 +0530

    Add files via upload

---
+++ src/Sections/Service.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { services } from '../constants'
+import ServiceCard from '../components/ServiceCard'
+const Service = () => {
+  return (
+    <section className='max-container flex justify-center flex-wrap gap-9'>
+      {services.map((service)=>(
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  )
+}
+
+export default Service
---
+++ src/Sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Service from "./Service";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Footer,
+    Service,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
